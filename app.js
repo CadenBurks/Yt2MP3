a8fd8bd37e944b0af005684ba26bc7efa28581c0
Caden Burks <cadendburks08@icloud.com>
2025-06-26 13:51:25 -0400

    Server setup with sample message

---
+++ app.js
@@ -0,0 +1,33 @@
+// necessary packages
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+// create express server
+const app = express();
+
+// server ports (heroku and local)
+const PORT = process.env.PORT || 3000;
+
+// set ejs template engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// info needed to parse html for POST request
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+app.post("/", (req, res) => {
+
+})
+
+// start server
+app.listen(PORT, () =>{
+    console.log(`Server started on port ${PORT}`);
+})
